a168ebf58fafb73752b05887598b7aee4b629317
Wang Qi <wqking@outlook.com>
2011-09-21 09:56:55 +0000

    git-svn-id: https://subversion.assembla.com/svn/kbasm/trunk/cpgf@3 5ea4cdcf-77fc-412b-903d-47c6f9a32d48

--- test/reflection/test_reflection_constructor.cpp
+++ test/reflection/test_reflection_constructor.cpp
@@ -29,11 +29,11 @@ public:
 	}
 
 	// 3
-	CLASS(string s) : ci(0), cs(s) {
+	CLASS(const string & s) : ci(0), cs(s) {
 	}
 
 	// 4
-	CLASS(int i, string s) : ci(i), cs(s) {
+	CLASS(int i, const string & s) : ci(i), cs(s) {
 	}
 
 	// 5
@@ -61,8 +61,8 @@ GMETA_DEFINE_CLASS(CLASS, CLASS, NAME_CLASS) {
 	reflectConstructor<void * ()>();
 	reflectConstructor<void * (const CLASS &)>();
 	reflectConstructor<void * (int)>();
-	reflectConstructor<void * (string)>();
-	reflectConstructor<void * (int, string)>();
+	reflectConstructor<void * (const string &)>();
+	reflectConstructor<void * (int, const string &)>();
 	reflectConstructor<void * (const CLASS_DATA &)>();
 	reflectConstructor<void * (const CLASS_DATA *)>();
 	reflectConstructor<void * (const GMetaVariadicParam *)>();
@@ -103,6 +103,360 @@ GTEST(Lib_Exists)
 	CTOR(8);
 	GCHECK(! ctor);
 }
+
+
+GTEST(API_Exists)
+{
+	GMetaScopedPointer<IMetaService> service(createMetaService());
+	GCHECK(service);
+
+	GMetaScopedPointer<IMetaClass> metaClass(service->findClassByName(NAME_CLASS));
+	GCHECK(metaClass);
+
+	GMetaScopedPointer<IMetaConstructor> ctor;
+
+	CTOR(0);
+	GCHECK(ctor);
+
+	CTOR(1);
+	GCHECK(ctor);
+
+	CTOR(2);
+	GCHECK(ctor);
+
+	CTOR(3);
+	GCHECK(ctor);
+
+	CTOR(4);
+	GCHECK(ctor);
+
+	CTOR(5);
+	GCHECK(ctor);
+
+	CTOR(6);
+	GCHECK(ctor);
+
+	CTOR(7);
+	GCHECK(ctor);
+
+	CTOR(8);
+	GCHECK(! ctor);
+}
+
+
+GTEST(Lib_ParamType)
+{
+	const GMetaClass * metaClass = findMetaClass(NAME_CLASS);
+	GCHECK(metaClass);
+	
+	const GMetaConstructor * ctor;
+
+	CTOR(0);
+	GCHECK(! ctor->isVariadic());
+	GEQUAL(ctor->getParamCount(), 0);
+
+	CTOR(1);
+	GCHECK(! ctor->isVariadic());
+	GEQUAL(ctor->getParamCount(), 1);
+	GEQUAL(ctor->getParamType(0), createMetaType<const CLASS &>());
+
+	CTOR(2);
+	GCHECK(! ctor->isVariadic());
+	GEQUAL(ctor->getParamCount(), 1);
+	GEQUAL(ctor->getParamType(0), createMetaType<int>());
+
+	CTOR(3);
+	GCHECK(! ctor->isVariadic());
+	GEQUAL(ctor->getParamCount(), 1);
+	GEQUAL(ctor->getParamType(0), createMetaType<string>());
+
+	CTOR(4);
+	GCHECK(! ctor->isVariadic());
+	GEQUAL(ctor->getParamCount(), 2);
+	GEQUAL(ctor->getParamType(0), createMetaType<int>());
+	GEQUAL(ctor->getParamType(1), createMetaType<const string &>());
+
+	CTOR(5);
+	GCHECK(! ctor->isVariadic());
+	GEQUAL(ctor->getParamCount(), 1);
+	GEQUAL(ctor->getParamType(0), createMetaType<const CLASS_DATA &>());
+
+	CTOR(6);
+	GCHECK(! ctor->isVariadic());
+	GEQUAL(ctor->getParamCount(), 1);
+	GEQUAL(ctor->getParamType(0), createMetaType<const CLASS_DATA *>());
+
+	CTOR(7);
+	GCHECK(ctor->isVariadic());
+	GEQUAL(ctor->getParamType(0), createMetaType<const GMetaVariadicParam *>());
+}
+
+
+GTEST(Lib_CheckParam)
+{
+	const GMetaClass * metaClass = findMetaClass(NAME_CLASS);
+	GCHECK(metaClass);
+	
+	const GMetaConstructor * ctor;
+
+	CTOR(0);
+	GCHECK(! ctor->checkParam(38, 0));
+	GCHECK(! ctor->checkParam("", 1));
+
+	CTOR(1);
+	GCHECK(ctor->checkParam(CLASS(), 0));
+	GCHECK(! ctor->checkParam("", 0));
+	GCHECK(! ctor->checkParam("", 1));
+
+	CTOR(2);
+	GCHECK(ctor->checkParam(19, 0));
+	GCHECK(! ctor->checkParam("", 0));
+	GCHECK(! ctor->checkParam("", 1));
+
+	CTOR(3);
+	GCHECK(ctor->checkParam("a", 0));
+	GCHECK(ctor->checkParam(string("bc"), 0));
+	GCHECK(! ctor->checkParam(3, 1));
+	GCHECK(! ctor->checkParam("", 1));
+
+	CTOR(4);
+	GCHECK(ctor->checkParam(19, 0));
+	GCHECK(ctor->checkParam("a", 1));
+	GCHECK(ctor->checkParam(string("bc"), 1));
+	GCHECK(! ctor->checkParam("", 0));
+	GCHECK(! ctor->checkParam(38, 1));
+
+	CTOR(5);
+	GCHECK(ctor->checkParam(CLASS_DATA(), 0));
+	GCHECK(! ctor->checkParam("", 0));
+	GCHECK(! ctor->checkParam("", 1));
+
+	CTOR(6);
+	GCHECK(ctor->checkParam((CLASS_DATA *)0, 0));
+	GCHECK(ctor->checkParam("abc", 0)); // dangerous
+	GCHECK(! ctor->checkParam("", 1));
+
+	CTOR(7);
+	GCHECK(ctor->checkParam("abc", 0));
+	GCHECK(ctor->checkParam(50, 1));
+	GCHECK(ctor->checkParam(50, 10));
+	GCHECK(ctor->checkParam(50, 100));
+	GCHECK(ctor->checkParam(50, 1000));
+}
+
+
+GTEST(API_CheckParam)
+{
+	GMetaScopedPointer<IMetaService> service(createMetaService());
+	GCHECK(service);
+
+	GMetaScopedPointer<IMetaClass> metaClass(service->findClassByName(NAME_CLASS));
+	GCHECK(metaClass);
+
+	GMetaScopedPointer<IMetaConstructor> ctor;
+
+	CTOR(0);
+	GCHECK(! metaCheckParam(ctor, 38, 0));
+	GCHECK(! metaCheckParam(ctor, "", 1));
+
+	CTOR(1);
+	GCHECK(metaCheckParam(ctor, CLASS(), 0));
+	GCHECK(! metaCheckParam(ctor, "", 0));
+	GCHECK(! metaCheckParam(ctor, "", 1));
+
+	CTOR(2);
+	GCHECK(metaCheckParam(ctor, 19, 0));
+	GCHECK(! metaCheckParam(ctor, "", 0));
+	GCHECK(! metaCheckParam(ctor, "", 1));
+
+	CTOR(3);
+	GCHECK(metaCheckParam(ctor, "a", 0));
+	GCHECK(metaCheckParam(ctor, string("bc"), 0));
+	GCHECK(! metaCheckParam(ctor, 3, 1));
+	GCHECK(! metaCheckParam(ctor, "", 1));
+
+	CTOR(4);
+	GCHECK(metaCheckParam(ctor, 19, 0));
+	GCHECK(metaCheckParam(ctor, "a", 1));
+	GCHECK(metaCheckParam(ctor, string("bc"), 1));
+	GCHECK(! metaCheckParam(ctor, "", 0));
+	GCHECK(! metaCheckParam(ctor, 38, 1));
+
+	CTOR(5);
+	GCHECK(metaCheckParam(ctor, CLASS_DATA(), 0));
+	GCHECK(! metaCheckParam(ctor, "", 0));
+	GCHECK(! metaCheckParam(ctor, "", 1));
+
+	CTOR(6);
+	GCHECK(metaCheckParam(ctor, (CLASS_DATA *)0, 0));
+	GCHECK(metaCheckParam(ctor, "abc", 0)); // dangerous
+	GCHECK(! metaCheckParam(ctor, "", 1));
+
+	CTOR(7);
+	GCHECK(metaCheckParam(ctor, "abc", 0));
+	GCHECK(metaCheckParam(ctor, 50, 1));
+	GCHECK(metaCheckParam(ctor, 50, 10));
+	GCHECK(metaCheckParam(ctor, 50, 100));
+	GCHECK(metaCheckParam(ctor, 50, 1000));
+}
+
+
+GTEST(Lib_Construct)
+{
+	const GMetaClass * metaClass = findMetaClass(NAME_CLASS);
+	GCHECK(metaClass);
+
+	const GMetaConstructor * ctor;
+
+	CLASS * pobj;
+
+	CTOR(0);
+	pobj = (CLASS * )ctor->invoke();
+	GEQUAL(pobj->ci, 0);
+	GEQUAL(pobj->cs, "");
+	metaClass->destroyInstance(pobj);
+	EXCEPT_META(ctor->invoke(1));
+
+	CTOR(1);
+	pobj = (CLASS * )ctor->invoke(CLASS(8, "clone me"));
+	GEQUAL(pobj->ci, 8);
+	GEQUAL(pobj->cs, "clone me");
+	metaClass->destroyInstance(pobj);
+	EXCEPT_META(ctor->invoke(CLASS(), ""));
+
+	CTOR(2);
+	pobj = (CLASS * )ctor->invoke(5);
+	GEQUAL(pobj->ci, 5);
+	GEQUAL(pobj->cs, "");
+	metaClass->destroyInstance(pobj);
+	EXCEPT_META(ctor->invoke(1, 2));
+
+	CTOR(3);
+	pobj = (CLASS * )ctor->invoke("haha");
+	GEQUAL(pobj->ci, 0);
+	GEQUAL(pobj->cs, "haha");
+	metaClass->destroyInstance(pobj);
+	EXCEPT_VARIANT(ctor->invoke(1));
+
+	CTOR(4);
+	pobj = (CLASS * )ctor->invoke(2, "another");
+	GEQUAL(pobj->ci, 2);
+	GEQUAL(pobj->cs, "another");
+	metaClass->destroyInstance(pobj);
+	EXCEPT_META(ctor->invoke());
+
+	CLASS_DATA data("oh", 38);
+
+	CTOR(5);
+	pobj = (CLASS * )ctor->invoke(data);
+	GEQUAL(pobj->ci, 38);
+	GEQUAL(pobj->cs, "oh");
+	metaClass->destroyInstance(pobj);
+	EXCEPT_META(ctor->invoke());
+
+	data = CLASS_DATA("?", 1999);
+
+	CTOR(6);
+	pobj = (CLASS * )ctor->invoke(&data);
+	GEQUAL(pobj->ci, 1999);
+	GEQUAL(pobj->cs, "?");
+	metaClass->destroyInstance(pobj);
+	EXCEPT_META(ctor->invoke());
+
+	CTOR(7);
+	pobj = (CLASS * )ctor->invoke();
+	GEQUAL(pobj->ci, 0);
+	GEQUAL(pobj->cs, "Sum");
+	metaClass->destroyInstance(pobj);
+	
+	CTOR(7);
+	pobj = (CLASS * )ctor->invoke(1, 2, 3, 5, 6, 7, 8, 9, 10);
+	GEQUAL(pobj->ci, 1 + 2 + 3 + 5 + 6 + 7 + 8 + 9 + 10);
+	GEQUAL(pobj->cs, "Sum");
+	metaClass->destroyInstance(pobj);
+}
+
+
+GTEST(API_Construct)
+{
+	GMetaScopedPointer<IMetaService> service(createMetaService());
+	GCHECK(service);
+
+	GMetaScopedPointer<IMetaClass> metaClass(service->findClassByName(NAME_CLASS));
+	GCHECK(metaClass);
+
+	GMetaScopedPointer<IMetaConstructor> ctor;
+
+	CLASS * pobj;
+
+	CTOR(0);
+	pobj = (CLASS * )metaCallConstructor(ctor);
+	GEQUAL(pobj->ci, 0);
+	GEQUAL(pobj->cs, "");
+	metaClass->destroyInstance(pobj);
+	EXCEPT_META(metaCallConstructor(ctor, 1));
+
+	CLASS clone(8, "clone me");
+
+	CTOR(1);
+	pobj = (CLASS * )metaCallConstructor(ctor, clone);
+	GEQUAL(pobj->ci, 8);
+	GEQUAL(pobj->cs, "clone me");
+	metaClass->destroyInstance(pobj);
+	EXCEPT_META(metaCallConstructor(ctor, clone, ""));
+
+	CTOR(2);
+	pobj = (CLASS * )metaCallConstructor(ctor, 5);
+	GEQUAL(pobj->ci, 5);
+	GEQUAL(pobj->cs, "");
+	metaClass->destroyInstance(pobj);
+	EXCEPT_META(metaCallConstructor(ctor, 1, 2));
+
+	CTOR(3);
+	pobj = (CLASS * )metaCallConstructor(ctor, "haha");
+	GEQUAL(pobj->ci, 0);
+	GEQUAL(pobj->cs, "haha");
+	metaClass->destroyInstance(pobj);
+	EXCEPT_META(metaCallConstructor(ctor, 1));
+
+	CTOR(4);
+	pobj = (CLASS * )metaCallConstructor(ctor, 2, "another");
+	GEQUAL(pobj->ci, 2);
+	GEQUAL(pobj->cs, "another");
+	metaClass->destroyInstance(pobj);
+	EXCEPT_META(metaCallConstructor(ctor));
+
+	CLASS_DATA data("oh", 38);
+
+	CTOR(5);
+	pobj = (CLASS * )metaCallConstructor(ctor, data);
+	GEQUAL(pobj->ci, 38);
+	GEQUAL(pobj->cs, "oh");
+	metaClass->destroyInstance(pobj);
+	EXCEPT_META(metaCallConstructor(ctor));
+
+	data = CLASS_DATA("?", 1999);
+
+	CTOR(6);
+	pobj = (CLASS * )metaCallConstructor(ctor, &data);
+	GEQUAL(pobj->ci, 1999);
+	GEQUAL(pobj->cs, "?");
+	metaClass->destroyInstance(pobj);
+	EXCEPT_META(metaCallConstructor(ctor));
+
+	CTOR(7);
+	pobj = (CLASS * )metaCallConstructor(ctor);
+	GEQUAL(pobj->ci, 0);
+	GEQUAL(pobj->cs, "Sum");
+	metaClass->destroyInstance(pobj);
+	
+	CTOR(7);
+	pobj = (CLASS * )metaCallConstructor(ctor, 1, 2, 3, 5, 6, 7, 8, 9, 10);
+	GEQUAL(pobj->ci, 1 + 2 + 3 + 5 + 6 + 7 + 8 + 9 + 10);
+	GEQUAL(pobj->cs, "Sum");
+	metaClass->destroyInstance(pobj);
+}
+
 
 
 
