81c0f26ef5850652b5ca319f3584e9d0066a7d65
Wang Qi <wqking@outlook.com>
2012-02-22 07:07:18 +0000

    git-svn-id: https://subversion.assembla.com/svn/kbasm/cpgf/trunk@208 5ea4cdcf-77fc-412b-903d-47c6f9a32d48

--- scripts/release.pl
+++ scripts/release.pl
@@ -18,6 +18,14 @@ my $patternList = [
 	{
 		%defaultConfig,
 
+		path => "",
+		files => [ 'readme.txt', 'license', 'changelog' ],
+		recursive => 0,
+	},
+
+	{
+		%defaultConfig,
+
 		path => "build",
 		files => [ '*' ],
 		recursive => 0,
@@ -32,10 +40,53 @@ my $patternList = [
 		recursive => 1,
 	},
 
+	{
+		%defaultConfig,
+
+		path => "src",
+		files => [ '*' ],
+		excludePath => [ 'game' ],
+		recursive => 1,
+	},
+
+	{
+		%defaultConfig,
+
+		path => "samples",
+		files => [ '*' ],
+		recursive => 1,
+	},
+
+	{
+		%defaultConfig,
+
+		path => "test",
+		files => [ '*' ],
+		recursive => 1,
+	},
+
+	{
+		%defaultConfig,
+
+		path => "scripts",
+		files => [ 'gen_pp.pl' ],
+		recursive => 0,
+	},
+
+	{
+		%defaultConfig,
+
+		path => "scripts/metagen",
+		files => [ '*' ],
+		excludePath => [ 'xml' ],
+		recursive => 1,
+	},
+
 ];
 
-
+my $scriptPath;
 my $home;
+my $releasePath;
 my $destPath;
 my $version;
 
@@ -45,17 +96,115 @@ sub doMain
 {
 	&usage unless($#ARGV == 1);
 
-	my $currentPath = &normalizePath(dirname($0));
+	$scriptPath = &normalizePath(dirname($0));
 
-	$home = $currentPath . '../';
-	$destPath = &normalizePath($ARGV[0]);
+	$home = $scriptPath . '../';
+	$releasePath = &normalizePath($ARGV[0]);
+	$destPath = $releasePath . 'cpgf/';
 	$version = $ARGV[1];
-
-	print $home;
 
 	foreach(@{$patternList}) {
 		&processPattern($_);
 	}
+
+	&makeZip;
+}
+
+sub makeZip
+{
+	chdir($releasePath);
+	
+	my $v = "$version";
+	$v =~ s/\./_/;
+	my $zipName = "cpgf_$v.zip";
+	system "zip -rq $zipName cpgf";
+}
+
+sub processPattern
+{
+	my ($pattern) = @_;
+
+	&doProcessPattern($pattern, $home . $pattern->{path}, $destPath . $pattern->{path});
+}
+
+sub doProcessPattern
+{
+	my ($pattern, $path, $dest) = @_;
+
+	$dest = &normalizePath($dest);
+
+	mkpath($dest);
+
+	$path = &normalizePath($path);
+
+	my @fileList = glob($path . '*');
+
+	foreach(@fileList) {
+		my $file = $_;
+		
+		my $destFileName = $dest . &getFileName($file);
+
+		if(-d $file) {
+			if($pattern->{recursive}) {
+				if(!&matchFileList($file, $pattern->{excludePath}))	{
+					&doProcessPattern($pattern, $file, $destFileName);
+				}
+			}
+
+			next;
+		}
+
+		next unless(matchFileList($file, $pattern->{files}));
+
+		if(-e $destFileName) {
+#			die "$destFileName exists.\n";
+		}
+
+		copy($file, $destFileName);
+		&processDestFile($pattern, $destFileName);
+	}
+}
+
+sub processDestFile
+{
+	my ($pattern, $destFile) = @_;
+
+	if($pattern->{licenseHead}) {
+		my $license = undef;
+
+		if($destFile =~ /\.(h|cpp)$/i) {
+			$license = 'licensehead_apache.txt';
+		}
+		elsif($destFile =~ /\.p[ml]$/i) {
+			$license = 'licensehead_apache_perl.txt';
+		}
+		if(defined($license)) {
+			system "perl $scriptPath/file_prefix.pl $scriptPath/$license $destFile";
+		}
+	}
+}
+
+sub matchFileList
+{
+	my ($file, $patternList) = @_;
+
+	foreach(@{$patternList}) {
+		if(&matchFile($file, $_)) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+sub matchFile
+{
+	my ($file, $pattern) = @_;
+
+	$pattern =~ s/\./\\./g;
+	$pattern =~ s/\*/\.\*/g;
+
+	return &getFileName($file) =~ m!^$pattern$!;
 }
 
 sub normalizePath
@@ -77,68 +226,6 @@ sub getFileName
 	$file =~ s!.*\\!!;
 
 	return $file;
-}
-
-sub processPattern
-{
-	my ($pattern) = @_;
-
-	&doProcessPattern($pattern, $pattern->{path}, $destPath . $pattern->{path});
-}
-
-sub doProcessPattern
-{
-	my ($pattern, $path, $dest) = @_;
-
-	$dest = &normalizePath($dest);
-
-	mkpath($dest);
-
-	$path = $home . $path;
-	$path = &normalizePath($path);
-
-	my @fileList = glob($path . '*');
-
-	foreach(@fileList) {
-		my $file = $_;
-		
-		my $destFileName = $dest . &getFileName($file);
-
-		if(-d $file) {
-			if($pattern->{recursive}) {
-				if(!&matchFileList($file, $pattern->{excludePath}))	{
-					&doProcessPattern($pattern, $file, $destFileName);
-				}
-			}
-
-			next;
-		}
-
-		copy($file, $dest);
-	}
-}
-
-sub matchFileList
-{
-	my ($file, $patternList) = @_;
-
-	foreach(@{$patternList}) {
-		if(&matchFile($file, $_)) {
-			return 1;
-		}
-	}
-
-	return 0;
-}
-
-sub matchFile
-{
-	my ($file, $pattern) = @_;
-
-	$pattern =~ s/\*/.*/g;
-	$pattern =~ s/\./\\\\./g;
-
-	return &getFileName($file) =~ $pattern;
 }
 
 sub usage
