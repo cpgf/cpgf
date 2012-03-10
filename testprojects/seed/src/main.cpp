80d365e08c0d471b2bdcecbfd72e77c4229e7f9d
Wang Qi <wqking@outlook.com>
2012-03-10 14:01:36 +0000

    git-svn-id: https://subversion.assembla.com/svn/kbasm/cpgf/trunk@235 5ea4cdcf-77fc-412b-903d-47c6f9a32d48

--- testprojects/seed/src/main.cpp
+++ testprojects/seed/src/main.cpp
@@ -4,11 +4,19 @@
 
 void testSer();
 
+void testBoost();
+
 int main(int argc, char * argv[])
 {
+	(void)argc;
+	(void)argv;
+
+//	testBoost();
+
 	testSer();
 
 //	_getch();
+	printf("\n");
 
 	return 0;
 }
--- testprojects/seed/src/ser.cpp
+++ testprojects/seed/src/ser.cpp
@@ -6,10 +6,15 @@
 #include "cpgf/gmetadefine.h"
 #include "cpgf/goutmain.h"
 
+#include "pinclude/gapiimpl.h"
+
+#include <string.h>
+
 #include <map>
 
 
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 
@@ -56,9 +61,9 @@ private:
 
 struct IMetaWriter : public IObject
 {
-	virtual void G_API_CC writeFundamental(const char * name, uint32_t archiveID, const GVariant & value) = 0;
+	virtual void G_API_CC writeFundamental(const char * name, uint32_t archiveID, const GVariantData * value) = 0;
 	virtual void G_API_CC writeString(const char * name, uint32_t archiveID, const char * value) = 0;
-	virtual void G_API_CC writeNullPointer(const char * name, uint32_t archiveID) = 0;
+	virtual void G_API_CC writeNullPointer(const char * name) = 0;
 
 	virtual void G_API_CC beginWriteObject(const char * name, uint32_t archiveID, void * instance, IMetaClass * metaClass) = 0;
 	virtual void G_API_CC endWriteObject(const char * name, uint32_t archiveID, void * instance, IMetaClass * metaClass) = 0;
@@ -69,28 +74,69 @@ struct IMetaWriter : public IObject
 //	virtual void G_API_CC endWriteArray(const char * name, uint32_t length, IMetaTypedItem * typeItem) = 0;
 };
 
+struct IMetaReader : public IObject
+{
+	virtual void G_API_CC readFundamental(const char * name, uint32_t * outArchiveID, GVariantData * outValue) = 0;
+	virtual char * G_API_CC readString(const char * name, uint32_t * outArchiveID) = 0;
+	virtual void G_API_CC freeString(char * s) = 0;
+	virtual void * G_API_CC readNullPointer(const char * name) = 0;
+
+	virtual void G_API_CC beginReadObject(const char * name, void * instance, IMetaClass * metaClass, uint32_t * outArchiveID) = 0;
+	virtual void G_API_CC endReadObject(const char * name, uint32_t archiveID, void * instance, IMetaClass * metaClass) = 0;
+
+	virtual uint32_t G_API_CC readReferenceID(const char * name, uint32_t archiveID) = 0;
+
+};
+
+
 namespace {
 
+enum PermanentType {
+	ptBool = 1,
+	ptChar = 2,
+	ptWchar = 3,
+	ptSignedChar = 4,
+	ptUnsignedChar = 5,
+	ptSignedShort = 6,
+	ptUnsignedShort = 7,
+	ptSignedInt = 8,
+	ptUnsignedInt = 9,
+	ptSignedLong = 10,
+	ptUnsignedLong = 11,
+	ptSignedLongLong = 12,
+	ptUnsignedLongLong = 13,
+	ptFloat = 14,
+	ptDouble = 15,
+	ptLongDouble = 16,
+	ptObject = 17,
+	ptString = 18,
+	ptWideString = 19,
+
+	ptNull = 50, // for null pointer
+	ptReferenceID = 51, // for one pointer reference to another
+
+};
+
 int defaultVariantTypeMap[] = {
-	vtBool, 0,
-	vtChar, 1,
-	vtWchar, 2,
-	vtSignedChar, 3,
-	vtUnsignedChar, 4,
-	vtSignedShort, 5,
-	vtUnsignedShort, 6,
-	vtSignedInt, 7,
-	vtUnsignedInt, 8,
-	vtSignedLong, 9,
-	vtUnsignedLong, 10,
-	vtSignedLongLong, 11,
-	vtUnsignedLongLong, 12,
-	vtFloat, 13,
-	vtDouble, 14,
-	vtLongDouble, 15,
-	vtObject, 16,
-	vtString, 17,
-	vtWideString, 18,
+	vtBool, ptBool,
+	vtChar, ptChar,
+	vtWchar, ptWchar,
+	vtSignedChar, ptSignedChar,
+	vtUnsignedChar, ptUnsignedChar,
+	vtSignedShort, ptSignedShort,
+	vtUnsignedShort, ptUnsignedShort,
+	vtSignedInt, ptSignedInt,
+	vtUnsignedInt, ptUnsignedInt,
+	vtSignedLong, ptSignedLong,
+	vtUnsignedLong, ptUnsignedLong,
+	vtSignedLongLong, ptSignedLongLong,
+	vtUnsignedLongLong, ptUnsignedLongLong,
+	vtFloat, ptFloat,
+	vtDouble, ptDouble,
+	vtLongDouble, ptLongDouble,
+	vtObject, ptObject,
+	vtString, ptString,
+	vtWideString, ptWideString,
 
 	vtEmpty,
 };
@@ -121,171 +167,376 @@ int getMappedTypeFromMap(int * variantTypeMap, GVariantType vt)
 	return -1;
 }
 
-template <typename STREAM>
-class GStreamMetaWriter : public IMetaWriter
+template <typename T>
+struct PermenentTypeMap
 {
+	typedef T Result;
+};
+
+template <>
+struct PermenentTypeMap <wchar_t>
+{
+	typedef short Result;
+};
+
+template <typename Stream, template<typename> class TypeMap >
+void streamWriteFundamental(Stream & stream, const GVariant & value)
+{
+	switch(value.getType()) {
+		case vtBool:
+			stream << fromVariant<typename TypeMap<bool>::Result>(value);
+			break;
+
+		case vtChar:
+			stream << fromVariant<typename TypeMap<char>::Result>(value);
+			break;
+
+		case vtWchar:
+			stream << fromVariant<typename TypeMap<wchar_t>::Result>(value);
+			break;
+
+		case vtSignedChar:
+			stream << fromVariant<typename TypeMap<signed char>::Result>(value);
+			break;
+
+		case vtUnsignedChar:
+			stream << fromVariant<typename TypeMap<unsigned char>::Result>(value);
+			break;
+
+		case vtSignedShort:
+			stream << fromVariant<typename TypeMap<signed short>::Result>(value);
+			break;
+
+		case vtUnsignedShort:
+			stream << fromVariant<typename TypeMap<unsigned short>::Result>(value);
+			break;
+
+		case vtSignedInt:
+			stream << fromVariant<typename TypeMap<signed int>::Result>(value);
+			break;
+
+		case vtUnsignedInt:
+			stream << fromVariant<typename TypeMap<unsigned int>::Result>(value);
+			break;
+
+		case vtSignedLong:
+			stream << fromVariant<typename TypeMap<signed long>::Result>(value);
+			break;
+
+		case vtUnsignedLong:
+			stream << fromVariant<typename TypeMap<unsigned long>::Result>(value);
+			break;
+
+		case vtSignedLongLong:
+			stream << fromVariant<typename TypeMap<signed long long>::Result>(value);
+			break;
+
+		case vtUnsignedLongLong:
+			stream << fromVariant<typename TypeMap<unsigned long long>::Result>(value);
+			break;
+
+		case vtFloat:
+			stream << fromVariant<typename TypeMap<float>::Result>(value);
+			break;
+
+		case vtDouble:
+			stream << fromVariant<typename TypeMap<double>::Result>(value);
+			break;
+
+		case vtLongDouble:
+			stream << fromVariant<typename TypeMap<long double>::Result>(value);
+			break;
+
+		default:
+			break;
+	}
+}
+
+template <typename Stream, template<typename> class TypeMap >
+GVariant streamReadFundamental(Stream & stream, GVariantType vt)
+{
+	switch(vt) {
+		case vtBool: {
+			typename TypeMap<bool>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtChar: {
+			typename TypeMap<char>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtWchar: {
+			typename TypeMap<wchar_t>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtSignedChar: {
+			typename TypeMap<signed char>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtUnsignedChar: {
+			typename TypeMap<unsigned char>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtSignedShort: {
+			typename TypeMap<signed short>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtUnsignedShort: {
+			typename TypeMap<unsigned short>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtSignedInt: {
+			typename TypeMap<signed int>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtUnsignedInt: {
+			typename TypeMap<unsigned int>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtSignedLong: {
+			typename TypeMap<signed long>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtUnsignedLong: {
+			typename TypeMap<unsigned long>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtSignedLongLong: {
+			typename TypeMap<signed long long>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtUnsignedLongLong: {
+			typename TypeMap<unsigned long long>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtFloat: {
+			typename TypeMap<float>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtDouble: {
+			typename TypeMap<double>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		case vtLongDouble: {
+			typename TypeMap<long double>::Result value;
+			stream >> value;
+			return value;
+		}
+
+		default:
+			break;
+	}
+}
+
+template <typename Stream, template<typename> class TypeMap = PermenentTypeMap>
+class GTextStreamMetaWriter : public ImplObject, public IMetaWriter
+{
+private:
+	enum DelimiterType {
+		dtNone, dtSpace, dtNewline
+	};
+
 public:
-	explicit GStreamMetaWriter(STREAM & stream) : stream(stream), variantTypeMap(defaultVariantTypeMap), referenceCount(1), indent(0) {
+	explicit GTextStreamMetaWriter(Stream & stream) : stream(stream), variantTypeMap(defaultVariantTypeMap), delimiter(dtNone) {
 	}
 
 protected:
-	virtual void G_API_CC writeFundamental(const char * name, uint32_t archiveID, const GVariant & value) {
-		this->writeIndent();
-		
-		this->stream << archiveID << "  " << name;
-		this->stream << " type: " << getMappedTypeFromMap(this->variantTypeMap, value.getType());
-		this->stream << " -- ";
+	IMPL_OBJECT
 
-		switch(value.getType()) {
-			case vtBool:
-				this->stream << fromVariant<bool>(value);
-				break;
+	virtual void G_API_CC writeFundamental(const char * name, uint32_t archiveID, const GVariantData * value) {
+		GVariant v(*value);
 
-			case vtChar:
-				this->stream << fromVariant<char>(value);
-				break;
+		this->writeType(getMappedTypeFromMap(this->variantTypeMap, v.getType()));
 
-			case vtWchar:
-				this->stream << fromVariant<wchar_t>(value);
-				break;
+		this->writeDelimiter();
 
-			case vtSignedChar:
-				this->stream << fromVariant<signed char>(value);
-				break;
-
-			case vtUnsignedChar:
-				this->stream << fromVariant<unsigned char>(value);
-				break;
-
-			case vtSignedShort:
-				this->stream << fromVariant<signed short>(value);
-				break;
-
-			case vtUnsignedShort:
-				this->stream << fromVariant<unsigned short>(value);
-				break;
-
-			case vtSignedInt:
-				this->stream << fromVariant<signed int>(value);
-				break;
-
-			case vtUnsignedInt:
-				this->stream << fromVariant<unsigned int>(value);
-				break;
-
-			case vtSignedLong:
-				this->stream << fromVariant<signed long>(value);
-				break;
-
-			case vtUnsignedLong:
-				this->stream << fromVariant<unsigned long>(value);
-				break;
-
-			case vtSignedLongLong:
-				this->stream << fromVariant<signed long long>(value);
-				break;
-
-			case vtUnsignedLongLong:
-				this->stream << fromVariant<unsigned long long>(value);
-				break;
-
-			case vtFloat:
-				this->stream << fromVariant<float>(value);
-				break;
-
-			case vtDouble:
-				this->stream << fromVariant<double>(value);
-				break;
-
-			case vtLongDouble:
-				this->stream << fromVariant<long double>(value);
-				break;
-
-			default:
-				break;
-		}
-		
-		this->stream << endl;
+		streamWriteFundamental<Stream, TypeMap>(this->stream, v);
 	}
 
 	virtual void G_API_CC writeString(const char * name, uint32_t archiveID, const char * value) {
-		this->writeIndent();
+		this->writeType(ptString);
 		
-		this->stream << archiveID << "  " << name << " -- " << value << endl;
+		this->writeDelimiter();
+
+		size_t len = strlen(value);
+
+		this->stream << static_cast<uint32_t>(len);
+
+		this->writeDelimiter();
+
+		this->stream.write(value, len);
 	}
 
-	virtual void G_API_CC writeNullPointer(const char * name, uint32_t archiveID) {
-		this->writeIndent();
-		
-		this->stream << archiveID << "  " << name << " -- " << "NULL" << endl;
+	virtual void G_API_CC writeNullPointer(const char * name) {
+		this->writeType(ptNull);
 	}
 
 	virtual void G_API_CC beginWriteObject(const char * name, uint32_t archiveID, void * instance, IMetaClass * metaClass) {
+		(void)name;
 		(void)instance;
 		(void)metaClass;
 
-		this->writeIndent();
-		
-		++this->indent;
+		if(this->delimiter != dtNone) {
+			this->stream << '\n';
+			this->delimiter = dtNone;
+		}
 
-		this->stream << archiveID << " beginObject  " << name << endl;
+		this->writeType(ptObject);
+		this->writeDelimiter();
+		this->stream << static_cast<uint32_t>(archiveID);
 	}
 
 	virtual void G_API_CC endWriteObject(const char * name, uint32_t archiveID, void * instance, IMetaClass * metaClass) {
+		(void)name;
 		(void)instance;
 		(void)metaClass;
 
-		--this->indent;
-		this->writeIndent();
-		
-		this->stream << archiveID << " endObject  " << name << endl;
+		this->delimiter == dtNewline;
 	}
 
 	virtual void G_API_CC writeReferenceID(const char * name, uint32_t archiveID, uint32_t referenceArchiveID) {
-		this->writeIndent();
+		this->writeType(ptReferenceID);
 		
-		this->stream << archiveID << " Reference  " << name << "  " << referenceArchiveID << endl;
+		this->writeDelimiter();
+		
+		this->stream << static_cast<uint32_t>(referenceArchiveID);
 	}
 
 protected:
-	void writeIndent() {
-		for(int i = 0; i < indent; ++i) {
-			this->stream << "    ";
+	void writeDelimiter() {
+		if(this->delimiter == dtNone) {
+			this->delimiter = dtSpace;
+		}
+		else {
+			if(this->delimiter == dtSpace) {
+				this->stream << ' ';
+			}
+			else {
+				this->stream << "\n";
+				this->delimiter = dtNone;
+			}
 		}
 	}
 
-	uint32_t G_API_CC unused_queryInterface(void *, void *)
-	{
-		return 0;
-	}
-
-	uint32_t G_API_CC addReference()
-	{
-		++this->referenceCount;
-
-		return this->referenceCount;
-	}
-
-	uint32_t G_API_CC releaseReference()
-	{
-		if(this->referenceCount > 0) {
-			--this->referenceCount;
-		}
-
-		unsigned int refCount = this->referenceCount;
-
-		if(this->referenceCount == 0) {
-			delete this;
-		}
-
-		return refCount;
+	void writeType(int permanentType) {
+		this->writeDelimiter();
+		this->stream << static_cast<uint16_t>(permanentType);
 	}
 
 private:
-	STREAM & stream;
+	Stream & stream;
 	int * variantTypeMap;
-	uint32_t referenceCount;
-	int indent;
+	DelimiterType delimiter;
+};
+
+template <typename Stream, template<typename> class TypeMap = PermenentTypeMap>
+class GTextStreamMetaReader : public ImplObject, public IMetaReader
+{
+public:
+	explicit GTextStreamMetaReader(Stream & stream) : stream(stream), variantTypeMap(defaultVariantTypeMap) {
+	}
+
+protected:
+	IMPL_OBJECT
+
+	virtual void G_API_CC readFundamental(const char * name, uint32_t * outArchiveID, GVariantData * outValue) {
+		PermanentType type = this->readType();
+		GVariantType vt = getVariantTypeFromMap(this->variantTypeMap, type);
+
+		GVariant v(streamReadFundamental<Stream, TypeMap>(this->stream, vt));
+		*outValue = v.takeData();
+	}
+
+	virtual char * G_API_CC readString(const char * name, uint32_t * outArchiveID) {
+		PermanentType type = this->readType();
+		uint32_t len;
+		this->stream >> len;
+		this->skipDelimiter();
+		char * s = new char[len + 1];
+		this->stream.read(s, len);
+		s[len] = 0;
+		return s;
+	}
+
+	virtual void G_API_CC freeString(char * s) {
+		delete s;
+	}
+
+	virtual void * G_API_CC readNullPointer(const char * name) {
+		PermanentType type = this->readType();
+		if(type == ptNull) {
+		}
+
+		return NULL;
+	}
+
+	virtual void G_API_CC beginReadObject(const char * name, void * instance, IMetaClass * metaClass, uint32_t * outArchiveID) {
+		(void)name;
+		(void)instance;
+		(void)metaClass;
+
+		PermanentType type = this->readType();
+		this->stream >> *outArchiveID;
+	}
+
+	virtual void G_API_CC endReadObject(const char * name, uint32_t archiveID, void * instance, IMetaClass * metaClass) {
+		(void)name;
+		(void)instance;
+		(void)metaClass;
+
+	}
+
+	virtual uint32_t G_API_CC readReferenceID(const char * name, uint32_t archiveID) {
+		PermanentType type = this->readType();
+		uint32_t referenceArchiveID;
+		this->stream >> referenceArchiveID;
+		return referenceArchiveID;
+	}
+
+protected:
+	void skipDelimiter() {
+		this->stream.get();
+	}
+
+	PermanentType readType() {
+		uint16_t type;
+		this->stream >> type;
+		this->skipDelimiter();
+		return static_cast<PermanentType>(type);
+	}
+
+private:
+	Stream & stream;
+	int * variantTypeMap;
 };
 
 
@@ -629,7 +880,8 @@ void GMetaArchiveWriter::doDirectWriteField(const char * name, void * instance, 
 
 	if(pointers == 0) {
 		if(metaType.isFundamental()) {
-			this->writer->writeFundamental(name, this->getNextArchiveID(), metaGetValue(accessible, instance));
+			GVariant v(metaGetValue(accessible, instance));
+			this->writer->writeFundamental(name, this->getNextArchiveID(), &v.data);
 		}
 		else if(metaType.baseIsClass()) {
 			if(metaType.getBaseName() != NULL) {
@@ -644,7 +896,7 @@ void GMetaArchiveWriter::doDirectWriteField(const char * name, void * instance, 
 		if(pointers == 1) {
 			void * ptr = fromVariant<void *>(metaGetValue(accessible, instance));
 			if(ptr == NULL) {
-				this->writer->writeNullPointer(name, this->getNextArchiveID());
+				this->writer->writeNullPointer(name);
 			}
 			else {
 				if(vtGetBaseType(metaType.getVariantType()) == vtChar) {
@@ -740,8 +992,10 @@ void testSer()
 {
 	GScopedInterface<IMetaService> service(createDefaultMetaService());
 
-	GStreamMetaWriter<ostream> stream(cout);
-	GMetaArchiveWriter archiveWriter(GMetaArchiveConfig(), service.get(), &stream);
+	stringstream stream;
+
+	GTextStreamMetaWriter<stringstream> outputStream(stream);
+	GMetaArchiveWriter archiveWriter(GMetaArchiveConfig(), service.get(), &outputStream);
 
 	GScopedInterface<IMetaClass> metaClass(service->findClassByName("TestClassSerialize"));
 
@@ -749,6 +1003,7 @@ void testSer()
 
 	TestClassSerialize instance;
 	TestClassSerialize anotherObject;
+	anotherObject.fieldInt = 98;
 	TestClassSerialize * pobj = &instance;
 	instance.fieldInt = 38;
 	instance.a = "abcdef";
@@ -759,5 +1014,11 @@ void testSer()
 //	archiveWriter.writeField(field->getName(), pobj, field.get());
 	
 	archiveWriter.writeObjectValue("obj", pobj, metaClass.get());
+
+	cout << stream.str().c_str();
+
+	stream.seekg(0);
+	
+	GTextStreamMetaReader<stringstream> inputStream(stream);
 }
 
