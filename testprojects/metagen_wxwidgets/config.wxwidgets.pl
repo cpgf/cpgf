26c87beab0c27179eca5fbadda2c9c7b428cda4b
Wang Qi <wqking@outlook.com>
2012-04-24 15:14:10 +0000

    git-svn-id: https://subversion.assembla.com/svn/kbasm/cpgf/trunk@313 5ea4cdcf-77fc-412b-903d-47c6f9a32d48

---
+++ testprojects/metagen_wxwidgets/config.wxwidgets.pl
@@ -0,0 +1,44 @@
+$config = {
+	projectID => 'wxwidgets',
+
+	headerOutput => 'cpp/include',
+	sourceOutput => 'cpp/src',
+	
+	metaOutputCallback => \&processCallback,
+	
+	cppNamespace => "meta_wxwidgets",
+	
+	metaClassFunctionPrefix => 'buildMetaClass_',
+	metaClassCreatePrefix => 'createMetaClass_',
+
+	metaClassMainRegisterPrefix => 'registerMain_',
+
+	sourceFilePrefix => 'meta_wxwidgets_',
+	mainSourceFile => 'register_meta_wxwidgets',
+
+	autoRegisterToGlobal => 1,
+	metaNamespace => 'wxwidgets',
+	sourceHeaderCode => '#include "wx/wx.h"',
+	sourceHeaderReplacer => \&doHeaderReplace,
+	metaHeaderPath => '',
+};
+
+sub processCallback
+{
+	my ($item, $callbackParam) = @_;
+
+	if($item->isMethod) {
+		if($item->getName eq 'GetMaxMotorForce') {
+			$callbackParam->{skipBind} = 1;
+		}
+	}
+}
+
+sub doHeaderReplace
+{
+	my ($fileName) = @_;
+
+	$fileName =~ s!.*Box2D[^/]*/Box2D!Box2D!i;
+
+	return $fileName;
+}
---
+++ testprojects/metagen_wxwidgets/metagen_wxwidgets.sh
@@ -0,0 +1,1 @@
+perl ../../tools/metagen/metagen.pl --xml xml/index.xml --config config.wxwidgets.pl
