74c024776c6a8a389cf5d9dbc75b2abfb23d7605
Wang Qi <wqking@outlook.com>
2012-05-01 11:58:24 +0000

    git-svn-id: https://subversion.assembla.com/svn/kbasm/cpgf/trunk@321 5ea4cdcf-77fc-412b-903d-47c6f9a32d48

--- tools/metagen/metagen_box2d/metagen_box2d.sh
+++ tools/metagen/metagen_box2d/metagen_box2d.sh
@@ -1,1 +1,2 @@
-java -cp "../tool/bin;../js.jar" metagen --config config.box2d.js --xml xml/index.xml
+java -cp ../js.jar -jar ../metagen.jar --config config.box2d.js --xml xml/index.xml
+#java -cp "../tool/bin;../js.jar" metagen --config config.box2d.js --xml xml/index.xml
---
+++ tools/metagen/tool/compile.bat
@@ -0,0 +1,4 @@
+javac -cp ../js.jar;./src -d ./bin ./src/metagen.java
+cd bin
+jar cmf ../metagen.mf metagen.jar metagen.class com
+mv metagen.jar ../../
