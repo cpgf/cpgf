b9fdac76ca934b8840d638926fe51f66a178017a
Wang Qi <wqking@outlook.com>
2012-06-14 06:40:14 +0000

    git-svn-id: https://subversion.assembla.com/svn/kbasm/cpgf/trunk@415 5ea4cdcf-77fc-412b-903d-47c6f9a32d48

---
+++ test/misc/test_sharedpointer.cpp
@@ -0,0 +1,155 @@
+#include "test_misc_common.h"
+
+#include "cpgf/gsharedptr.h"
+#include "cpgf/gbytearray.h"
+#include "cpgf/gbytearrayapi.h"
+#include "cpgf/gvariant.h"
+
+#include <vector>
+
+
+using namespace cpgf;
+using namespace std;
+
+
+namespace Test_SharedPointer { namespace {
+
+class TestData
+{
+public:
+	explicit TestData(int * counter) : counter(counter) {
+		++*counter;
+	}
+
+	~TestData() {
+		--*counter;
+	}
+
+private:
+	int * counter;
+};
+
+typedef GSharedPointer<TestData> SP;
+
+class TestObject
+{
+public:
+	TestObject() {}
+
+	explicit TestObject(int * counter) : data(new TestData(counter)) {
+	}
+
+	~TestObject() {
+	}
+
+private:
+	SP data;
+};
+
+
+GTEST(TestSharedPointer_Simple)
+{
+	int count = 0;
+
+	{
+		SP sp(new TestData(&count));
+		GEQUAL(1, count);
+	}
+	GEQUAL(0, count);
+
+	{
+		SP sp1(new TestData(&count));
+		GEQUAL(1, count);
+
+		SP sp2(new TestData(&count));
+		GEQUAL(2, count);
+		
+		sp2 = sp1;
+		GEQUAL(1, count);
+	}
+	GEQUAL(0, count);
+
+	{
+		SP sp1(new TestData(&count));
+		GEQUAL(1, count);
+
+		SP sp2 = sp1;
+		GEQUAL(1, count);
+		
+		SP sp3;
+		GEQUAL(1, count);
+		
+		sp3 = sp2;
+		GEQUAL(1, count);
+		
+		sp1 = sp3;
+		GEQUAL(1, count);
+	}
+	GEQUAL(0, count);
+}
+
+
+GTEST(TestSharedPointer_Object)
+{
+	int count = 0;
+
+	{
+		TestObject obj(&count);
+		GEQUAL(1, count);
+	}
+	GEQUAL(0, count);
+
+	{
+		TestObject obj1(&count);
+		GEQUAL(1, count);
+
+		TestObject obj2(&count);
+		GEQUAL(2, count);
+		
+		obj1 = obj2;
+		GEQUAL(1, count);
+	}
+	GEQUAL(0, count);
+
+	{
+		TestObject obj1(&count);
+		GEQUAL(1, count);
+
+		TestObject obj2 = obj1;
+		GEQUAL(1, count);
+		
+		TestObject obj3;
+		GEQUAL(1, count);
+		
+		obj3 = obj2;
+		GEQUAL(1, count);
+		
+		obj1 = obj3;
+		GEQUAL(1, count);
+	}
+	GEQUAL(0, count);
+}
+
+
+GTEST(TestSharedPointer_DataInvector)
+{
+	const int iteral = 100;
+
+	int count = 0;
+
+	{
+		vector<SP> v;
+
+		for(int i = 0; i <iteral; ++i) {
+			SP sp(new TestData(&count));
+			v.push_back(sp);
+		}
+		GEQUAL(iteral, count);
+    }
+	GEQUAL(0, count);
+}
+
+
+
+} }
+
