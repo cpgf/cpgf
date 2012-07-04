a3857ebeda1b16f2bb1307f641964c1b2350ebb7
Wang Qi <wqking@outlook.com>
2012-07-04 10:45:18 +0000

    git-svn-id: https://subversion.assembla.com/svn/kbasm/cpgf/trunk@441 5ea4cdcf-77fc-412b-903d-47c6f9a32d48

--- samples/tutorials/a03_interface_api_use_global_metadata.cpp
+++ samples/tutorials/a03_interface_api_use_global_metadata.cpp
@@ -50,55 +50,74 @@ public:
 
 void run_a03()
 {
+	// Now let's see how to use interface based API.
+	//
+	// The APIs we used in tutorial a01 and a02, such as GMetaClass, GMetaField, etc, are class and function based API.
+	// They are normal native C++ classes and functions.
+	// The problem of the classes and functions, they are not binary compatible across dynamic libraries and compilers.
+	// It's not easy, or even not possible, to use classes in another dynamic library compiled by another compiler.
+	//
+	// That's why interface based API was developed.
+	// All interface based APIs are using interface. Or to say, Windows COM (Component Object Model) style interfaces.
+	// All interfaces have similar name with their class counterpart. IMetaClass is the interface for GMetaClass, etc.
+	//
+	// Important reminding, interface based APIs are NOT optional.
+	// These kind of APIs are the most important APIs in cpgf library.
+	// Also, the serialization and script binding libraries, are developed upon interface based APIs.
+	//
+	// Now let's start using interface based API.
+	// First let's get an instance of IMetaService.
+	// IMetaService is a center to obtain all meta data in the system.
+	// We call createDefaultMetaService() to get a service to represent the global meta repository.
+	// NOTE: All interface instance returned by cpgf must be freed (releaseReference) by the caller.
+	// The simplest way to do it is to use GScopedInterface to hold the instance.
 	GScopedInterface<IMetaService> service(createDefaultMetaService());
-
-	GScopedInterface<IMetaModule> globalModule(service->getModuleAt(0));
-	GScopedInterface<IMetaClass> pointMetaClass(service->findClassByName("Point"));
 
 	GScopedInterface<IMetaMethod> method;
 	GScopedInterface<IMetaField> field;
 	GVariant value;
 
+	// Get the global meta class to use the function "greeting".
+	// The service returned by createDefaultMetaService will only hold one module to represent current program.
+	// The meta service is designed to work on multiple dynamic libraries.
+	// A dynamic library (include the program itself), is called a module.
+	// Each module has only one meta repository.
+	// Please bear in mind, cpgf was not tested in cross dynamic library environment yet, so the service/module
+	// architecture may not be matured.
+	GScopedInterface<IMetaModule> globalModule(service->getModuleAt(0));
 	GScopedInterface<IMetaClass> globalMetaClass(globalModule->getGlobalMetaClass());
 	method.reset(globalMetaClass->getMethod("greeting"));
 	metaInvokeMethod(method, NULL, "world");
 
+	GScopedInterface<IMetaClass> pointMetaClass(service->findClassByName("Point"));
+
 	Point point(5, 8);
 
-	// Get the meta field for "x".	
 	field.reset(pointMetaClass->getField("x"));
-	// Get the value of "x". The result is a GVariant.
+	// Here we get value from the field.
+	// Unlike the class and function based API which passes GVariant and GMetaType around,
+	// The interface based API uses GVariantData and GMetaTypeData to gain the binary compatibility.
+	// Fortunately, in the header gmetaapiutil.h, there are a bunch of utility functions to hide the conversion
+	// between GVariant and GVariantData, GMetaType and GMetaTypeData.
 	value = metaGetValue(field, &point);
-	// Print the value. We use fromVariant to convert a GVariant to relevant C++ type.
 	cout << "point.x is " << fromVariant<int>(value) << " (should be 5)" << endl;
 
-	// Get the meta method for "extend".
 	method.reset(pointMetaClass->getMethod("extend"));
-	// Invoke Point::extend.
+	// Another handy utility function to invoke the method.
 	metaInvokeMethod(method, &point, 2);
 	cout << "After extend, point.x is " << fromVariant<int>(metaGetValue(field, &point)) << " (should be 10)" << endl;
 
 
-	// Get the meta method for "getArea".
 	method.reset(pointMetaClass->getMethod("getArea"));
-	// Invoke Point::getArea, and obtain the return value.
 	value = metaInvokeMethod(method, &point);
-	// Print the return value.
 	cout << "The area is " << fromVariant<int>(value) << " (should be 160)" << endl;
-
-	// Now let's learn something about constructing object and meta constructor.
 
 	Point * p;
 
-	// Create an instance with default constructor.
-	// createInstance doesn't need the default ctor to be reflected.
 	p = static_cast<Point *>(pointMetaClass->createInstance());
 	cout << "createInstance -- point.x is " << p->x << " (should be 0)" << endl;
-	// Don't forget to free the object once we don't need it.
 	pointMetaClass->destroyInstance(p);
 
-	// Get the meta constructor of void *(int, int). 0 is the index.
-	// If there is another meta constructor in front of this one in the meta data, the index should be 1.
 	GScopedInterface<IMetaConstructor> constructor(pointMetaClass->getConstructorAt(0));
 	p = static_cast<Point *>(metaInvokeConstructor(constructor, 3, 8));
 	cout << "constructor -- point.x is " << p->x << " (should be 3)" << endl;
