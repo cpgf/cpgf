4a98ec01e90a8496b843510e607a72677bc5493f
Wang Qi <wqking@outlook.com>
2012-11-23 08:37:20 +0000

    git-svn-id: https://subversion.assembla.com/svn/kbasm/cpgf/trunk@651 5ea4cdcf-77fc-412b-903d-47c6f9a32d48

--- build/qtest.sh
+++ build/qtest.sh
@@ -1,9 +1,15 @@
+BUILD_TARGET=$1
+if [ -z "$BUILD_TARGET" ]; then
 #BUILD_TARGET=test_serialization
 #BUILD_TARGET=test_scriptbind
 #BUILD_TARGET=test_reflection
-BUILD_TARGET=test_misc
-#BUILD_TARGET=test_metagen
+#BUILD_TARGET=test_misc
+BUILD_TARGET=test_metagen
+#BUILD_TARGET=test_variant
 #BUILD_TARGET=tutorials
+fi
+
+echo BULD TARGET $BUILD_TARGET
 
 if [ $OSTYPE == "cygwin" ]; then
 	mingw32-make mingw TARGET=$BUILD_TARGET
@@ -13,10 +19,10 @@ fi
 
 if [ $? != 0 ]; then
 	echo Error occurred.
-	exit 1
+	exit $?
 fi
 
 cd ./bin
 ./$BUILD_TARGET
-cd ../
 
+exit $?
