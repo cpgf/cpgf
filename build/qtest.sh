5700395dc253bb370ff2104515212ddbd00ff8d8
Wang Qi <wqking@outlook.com>
2013-04-29 08:51:34 +0000

    git-svn-id: https://subversion.assembla.com/svn/kbasm/cpgf/trunk@691 5ea4cdcf-77fc-412b-903d-47c6f9a32d48

--- build/qtest.sh
+++ build/qtest.sh
@@ -4,9 +4,12 @@ if [ -z "$BUILD_TARGET" ]; then
 #BUILD_TARGET=test_scriptbind
 #BUILD_TARGET=test_reflection
 #BUILD_TARGET=test_misc
-BUILD_TARGET=test_metagen
+#BUILD_TARGET=test_metagen
 #BUILD_TARGET=test_variant
 #BUILD_TARGET=tutorials
+#BUILD_TARGET=test_metadata_stl
+#BUILD_TARGET=test_accessor
+BUILD_TARGET=test_tween
 fi
 
 echo BULD TARGET $BUILD_TARGET
