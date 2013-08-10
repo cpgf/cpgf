140404f97457812cabed246874299ada3a6c30d1
Wang Qi <wqking@outlook.com>
2013-08-10 13:36:35 +0000

    git-svn-id: https://subversion.assembla.com/svn/kbasm/cpgf/trunk@817 5ea4cdcf-77fc-412b-903d-47c6f9a32d48

--- test/metagen/tests/luatests/test_metagen_lua_inherit_from_script.cpp
+++ test/metagen/tests/luatests/test_metagen_lua_inherit_from_script.cpp
@@ -39,4 +39,29 @@ void testInheritFromScript_SimpleObject(TestScriptContext * context)
 #include "../do_testcase_lua.h"
 
 
+// This test is to hide C++ method from script, it's not overriding. That's to say
+// calling the method from C++ will call the C++ method, not the script method.
+// To use overriding, we have to use class wrapper.
+void testInheritFromScript_SimpleObject_HideCppMethod(TestScriptContext * context)
+{
+	DO(codeInheritsFrom);
+	DO("DerivedSimpleObject = inheritsFrom(mtest.SimpleObject)");
+	QNEWOBJ(a, DerivedSimpleObject:create());
+
+	// Hide all C++ methods checkData
+	QDO(a.checkData = function(d) return d == "overrided" end);
+
+	QDO(a.getData().n = 78);
+	QDO(a.getData().s = "def");
+	QDO(a.getData().atom.value = 98);
+	QASSERT(not a.checkData(78));
+	QASSERT(not a.checkData("def"));
+	QASSERT(a.checkData("overrided"));
+}
+
+#define CASE testInheritFromScript_SimpleObject_HideCppMethod
+#include "../do_testcase_lua.h"
+
+
+
 }
--- test/metagen/tests/spidermonkeytests/test_metagen_spidermonkey_inherit_from_script.cpp
+++ test/metagen/tests/spidermonkeytests/test_metagen_spidermonkey_inherit_from_script.cpp
@@ -23,5 +23,30 @@ void testInheritFromScript_SimpleObject(TestScriptContext * context)
 #include "../do_testcase_spidermonkey.h"
 
 
+/*
+// This test is to hide C++ method from script, it's not overriding. That's to say
+// calling the method from C++ will call the C++ method, not the script method.
+// To use overriding, we have to use class wrapper.
+void testInheritFromScript_SimpleObject_HideCppMethod(TestScriptContext * context)
+{
+	DO("function DerivedSimpleObject() {}");
+	QDO(DerivedSimpleObject.prototype = new mtest.SimpleObject());
+	QNEWOBJ(a, DerivedSimpleObject());
+
+	// Hide all C++ methods checkData
+	QDO(a.checkData = function(d) { return d == "overrided"; });
+
+	QDO(a.getData().n = 78);
+	QDO(a.getData().s = "def");
+	QDO(a.getData().atom.value = 98);
+	QNOT(a.checkData(78));
+	QNOT(a.checkData("def"));
+	QASSERT(a.checkData("overrided"));
+}
+
+#define CASE testInheritFromScript_SimpleObject_HideCppMethod
+#include "../do_testcase_spidermonkey.h"
+*/
+
 
 }
