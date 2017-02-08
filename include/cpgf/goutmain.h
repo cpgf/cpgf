dd48e62ee865d6b12104ea18f0bb5c346b67c5e3
Wang Qi <wqking@outlook.com>
2017-02-08 13:47:58 +0800

    Added optional name parameter to G_AUTO_RUN_BEFORE_MAIN so there will not be link error if the two macros are on same line number.

--- include/cpgf/goutmain.h
+++ include/cpgf/goutmain.h
@@ -9,7 +9,7 @@
 	static struct GPP_CONCAT(I_rUnBeforemAin_, __LINE__) { GPP_CONCAT(I_rUnBeforemAin_, __LINE__)() {__VA_ARGS__ ;} } GPP_CONCAT(v_rUnBeforemAin_, __LINE__); \
 	}
 
-#define G_AUTO_RUN_BEFORE_MAIN() I_G_AUTO_RUN_BEFORE_MAIN(__LINE__)
+#define G_AUTO_RUN_BEFORE_MAIN(...) I_G_AUTO_RUN_BEFORE_MAIN(__VA_ARGS__ ## __LINE__)
 
 #define I_G_AUTO_RUN_BEFORE_MAIN(ln) \
 	void GPP_CONCAT(I_AutorUnBeforemAin_, ln)(); \
