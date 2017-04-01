751ba9b4f1f241ba3cec6e0f0fc117e0943cd663
wqking <wqking@outlook.com>
2017-04-01 15:07:34 +0800

    Removed unnecessary code.

--- src/scriptbind/gbindcontext.cpp
+++ src/scriptbind/gbindcontext.cpp
@@ -239,11 +239,13 @@ GObjectInstancePointer GClassPool::findObjectData(const GVariant & instance)
 		GObjectInstancePointer data(it->second.get());
 		return data;
 	}
+/* no need this check because objectAddressFromVariant == getInstanceHash
 	it = this->instanceMap.find(objectAddressFromVariant(instance));
 	if(it != instanceMap.end() && it->second) {
 		GObjectInstancePointer data(it->second.get());
 		return data;
 	}
+*/
 	return GObjectInstancePointer();
 }
 
